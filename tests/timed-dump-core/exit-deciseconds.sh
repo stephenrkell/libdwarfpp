749c6a3f3246c40b598be88382d91d503ceb1a17
Stephen Kell <srk31@cl.cam.ac.uk>
2014-06-20 13:41:04 +0100

    tests/timed-dump-core: re-enable debugging printout as it's generally useful.

--- tests/timed-dump-core/exit-deciseconds.sh
+++ tests/timed-dump-core/exit-deciseconds.sh
@@ -2,6 +2,6 @@
 
 deciseconds=$( /usr/bin/time -f "%U" "$@" 2>&1 | sed 's/\.\([0-9]\)[0-9]*$/\1/' )
 
-# echo deciseconds is $deciseconds 1>&2
+echo deciseconds is $deciseconds 1>&2
 
 exit $deciseconds
