ff08fff9dbee59725c84dac181edcf53decb4f2f
Stephen Kell <srk31@srcf.ucam.org>
2023-11-23 16:35:58 +1100

    tests/timed-dump-core: measure in centiseconds (fast machines eh)

--- tests/timed-dump-core/exit-deciseconds.sh
+++ tests/timed-dump-core/exit-deciseconds.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-deciseconds=$( /usr/bin/time -f "%U" "$@" 2>&1 | sed 's/\.\([0-9]\)[0-9]*$/\1/' )
+# 'fracseconds'
+case "$(basename "$0")" in
+	(*deciseconds*) ord=1 ;;
+	(*centiseconds*) ord=2 ;;
+	(*) echo "Did not understand `basename $0`" 1>&2; exit 1 ;;
+esac
 
-echo deciseconds is $deciseconds 1>&2
+timefile=`mktemp`
+/usr/bin/time -f "%U" "$@" > "$timefile" 2>&1
+status=$?
+fracseconds="$( cat "$timefile" | sed "s/\.\([0-9]\)\{${ord}\}[0-9]*\$/\1/" )"
+rm -f "$timefile"
 
-exit $deciseconds
+echo status is $status 1>&2
+echo fracseconds is $fracseconds 1>&2
+
+exit $fracseconds
--- tests/timed-dump-core/timed-dump-core.cpp
+++ tests/timed-dump-core/timed-dump-core.cpp
@@ -13,17 +13,17 @@ int main(int argc, char **argv)
 	std::ifstream in(argv[0]);
 	core::root_die root(fileno(in));
 
-	int status0 = system("./exit-deciseconds.sh sh -c '../../contrib/libdwarf/prefix/bin/dwarfdump ../../examples/dwarfppdump >/dev/null 2>/dev/null'");
-	int status1 = system("./exit-deciseconds.sh sh -c 'readelf -wi ../../examples/dwarfppdump >/dev/null 2>/dev/null'");
-	int status2 = system("./exit-deciseconds.sh sh -c '../../examples/dwarfppdump ../../examples/dwarfppdump >/dev/null 2>/dev/null'");
+	int status0 = system("./exit-centiseconds.sh sh -c '../../contrib/libdwarf/prefix/bin/dwarfdump ../../examples/dwarfppdump >/dev/null 2>/dev/null'");
+	int status1 = system("./exit-centiseconds.sh sh -c 'readelf -wi ../../examples/dwarfppdump >/dev/null 2>/dev/null'");
+	int status2 = system("./exit-centiseconds.sh sh -c '../../examples/dwarfppdump ../../examples/dwarfppdump >/dev/null 2>/dev/null'");
 	
 	int exit_status0 = WEXITSTATUS(status0);
 	int exit_status1 = WEXITSTATUS(status1);
 	int exit_status2 = WEXITSTATUS(status2);
 	
-	cout << "Deciseconds for dwarfdump: " << exit_status0 << endl;
-	cout << "Deciseconds for readelf: " << exit_status1 << endl;
-	cout << "Deciseconds for our code: " << exit_status2 << endl;
+	cout << "Centiseconds for dwarfdump: " << exit_status0 << endl;
+	cout << "Centiseconds for readelf: " << exit_status1 << endl;
+	cout << "Centiseconds for our code: " << exit_status2 << endl;
 	
 	/* This is only likely to pass for an optimised build. */
 	assert(exit_status2 < 1.4 * exit_status0); 
